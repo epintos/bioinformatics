65ebbc272314b47cac9270df09af5251cb7163b6
Esteban Pintos <epintos7@gmail.com>
2013-10-17 00:41:46 -0300

    Trying to parse blast report

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ Bioinformatics
 =============
 
 First project for Bioinformatics course at ITBA. Year 2013.
+
+Instructions
+============
+
+* `Ex1.rb` Generates a Fasta sequence file from a GenBank input file
+  * `ruby src/Ex1.rb input_file_path output_file_path`
+  * `input_file_path` is the file path for a GenBank input file
+  * `output_file_path` is the file path for the fasta output file
+  * Example: `ruby src/Ex1.rb files/genbank/tritricum_globulin.gbk outputs/Ex1.fas`
+
+* `Ex2.rb` Generates a Blast report from a Fasta file
+  * `ruby src/Ex2.rb input_file_path output_file_path type local_or_remote`
+  * `input_file_path` is the file path for the fasta input file
+  * `output_file_path` is the file path for the blast report output file
+  * `type` is either `--prot` or `--nuc`
+  * `local_or_remote` is either `--local` or `--remote`
+  * Example: `ruby src/Ex2.rb files/fasta/maize_globulin.fas outputs/Ex2.out --nuc --remote`
+
+
+  
--- src/Ex3.rb
+++ src/Ex3.rb
@@ -1,11 +1,31 @@
 #!/usr/bin/env ruby
-
-puts 'Exercise 3'
+#http://bioruby.org/rdoc/Bio/NCBI/REST/ESearch/Methods.html
+#https://gist.github.com/jprewitt/1655444
 require 'bio'
-require 'rubygems'
 
-gb = Bio::GenBank.new('P02170.2')
-file = File.open("outputs/exercise3.out", "w+") 
-puts gb.seq_len
-file.puts gb.to_biosequence
-file.close
+
+if ARGV.length != 3
+  puts 'Invalid params: 1) In file path - 2) Pattern - 3) Out file path'
+  exit
+end
+
+pattern = ARGV[1]
+Bio::NCBI.default_email = "@"
+genbank_id = Bio::NCBI::REST::ESearch.search("nucleotide", "168480").first
+genbank_string = Bio::NCBI::REST::EFetch.nucleotide(genbank_id)
+# genbank_id = Bio::NCBI::REST::ESearch.search("nucleotide", "118595760").first
+# genbank_string = Bio::NCBI::REST::EFetch.nucleotide(genbank_id)
+
+genbank_parsed = Bio::GenBank.new(genbank_string)
+
+Bio::Blast.reports(ARGV[0]) do |report|
+  # puts "Hits for " + report.query_def + " against " + report.db
+  # report.each do |hit|
+    # print hit.target_id, " -> ", hit.evalue if hit.evalue < 0.001
+  # end
+end
+
+File.open(ARGV[2], 'w') do |f|
+  f.puts(genbank_parsed.definition)
+  f.puts(genbank_parsed.seq)
+end
